1db35bad08c6426a819c50bbdfeec90a32b1c0a7
migzcervvv <migzcerv007@gmail.com>
2026-09-14 21:07:31 +0800

    Added migration

---
+++ JobTracker.Api/Migrations/20260914114024_AddResumeExtractionFields.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResumeExtractionFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExtractionStatus",
+                table: "Resumes",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProposedSkillsJson",
+                table: "Resumes",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExtractionStatus",
+                table: "Resumes");
+
+            migrationBuilder.DropColumn(
+                name: "ProposedSkillsJson",
+                table: "Resumes");
+        }
+    }
+}
